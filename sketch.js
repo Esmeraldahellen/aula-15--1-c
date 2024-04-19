f12aa6fe40050347ecec6dcdb22bc16744ccc8d0
Esmeraldahellen <166130092+Esmeraldahellen@users.noreply.github.com>
2024-04-19 09:17:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+  function setup() {
+   createCanvas(600,400);
+   background("pink"); //função para pintar o fundo
+}
+
+function draw() {
+  stroke ("green");//alterando a cor da borda do desenho
+  fill("red");//alterar a cor de preenchimento do desenho
+}
+
+  if(mouelsPressed){
+  
+    
+      rect(mouseX,mouseY,80,40)
+     }
+   }
